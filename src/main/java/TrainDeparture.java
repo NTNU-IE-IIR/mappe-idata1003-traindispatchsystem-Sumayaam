8ca9fcaac89b004f532f26931c32f8422cf8f9c5
Sumaya Mohammud <sumayamohammud@dhcp-10-22-176-41.wlan.ntnu.no>
2023-10-17 11:15:31 +0200

    lagde klassen Traindeparture, og fields

---
+++ src/main/java/TrainDeparture.java
@@ -0,0 +1,8 @@
+public class TrainDeparture {
+    private String departureTime;
+    private String line;
+    private String trainNumber;
+    private String destination;
+    private int track;
+
+}
--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -4,7 +4,6 @@ package edu.ntnu.stud;
  * This is the main class for the train dispatch application.
  */
 public class TrainDispatchApp {
-  // TODO: Fill in the main method and any other methods you need.
   public static void main(String[] args) {
   }
 }
