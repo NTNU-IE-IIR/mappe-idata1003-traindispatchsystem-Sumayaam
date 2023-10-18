0b41a2ef1f81b0de948a07bce47dfb8534651a18
Sumaya Mohammud <sumayamohammud@dhcp-10-22-176-41.wlan.ntnu.no>
2023-10-18 11:36:12 +0200

    laget en konstrukør for klassen, og lagt til objektet 'delay'

--- src/main/java/TrainDeparture.java
+++ src/main/java/TrainDeparture.java
@@ -1,8 +1,22 @@
+import java.time.LocalTime;
 public class TrainDeparture {
-    private String departureTime;
+    private LocalTime departureTime;
     private String line;
     private String trainNumber;
     private String destination;
+    private LocalTime delay;
     private int track;
 
+    public TrainDeparture(LocalTime departureTime, String line, String trainNumber, String destination,LocalTime delay,  int track) {
+        this.departureTime = departureTime;
+        this.line = line;
+        this.trainNumber = trainNumber;
+        this.destination = destination;
+        this.delay = delay;
+        this.track = track;
+    }
+
+
+
 }
+
