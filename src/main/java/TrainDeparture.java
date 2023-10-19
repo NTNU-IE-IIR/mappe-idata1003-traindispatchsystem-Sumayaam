04919a79c180aef610af30f95af47eb9cdc964b8
Sumaya Mohammud <sumayamohammud@Sumaya-sin-MacBook-Pro.local>
2023-10-19 20:42:03 +0200

    lagt til kommentar

--- src/main/java/TrainDeparture.java
+++ src/main/java/TrainDeparture.java
@@ -1,4 +1,5 @@
 import java.time.LocalTime;
+// define the Class TrainDeparture
 public class TrainDeparture {
     private LocalTime departureTime;
     private String line;
@@ -6,8 +7,10 @@ public class TrainDeparture {
     private String destination;
     private LocalTime delay;
     private int track;
-
-    public TrainDeparture(LocalTime departureTime, String line, String trainNumber, String destination,LocalTime delay,  int track) {
+//Define the constructor
+    public TrainDeparture(LocalTime departureTime, String line, String trainNumber, String destination, LocalTime delay,
+                          int track)
+    {
         this.departureTime = departureTime;
         this.line = line;
         this.trainNumber = trainNumber;
@@ -15,8 +18,37 @@ public class TrainDeparture {
         this.delay = delay;
         this.track = track;
     }
+// create accsessor methods to get information
+    public LocalTime getDepartureTime()
+    {
+        return departureTime;
+    }
 
+    public String getLine()
+    {
+        return line;
+    }
 
+    public String getTrainNumber()
+    {
+        return trainNumber;
+    }
 
+    public String getDestination()
+    {
+        return destination;
+    }
+
+    public LocalTime getDelay()
+    {
+        return delay;
+    }
+
+    public int getTrack() {
+        return track;
+    }
 }
+
+
+
 
