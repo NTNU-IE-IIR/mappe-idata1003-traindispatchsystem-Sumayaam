07f872f454a6ef965b223ce5979256e379645a21
Sumaya Mohammud <sumayamohammud@Sumaya-sin-MacBook-Pro.local>
2023-11-02 15:05:53 +0100

    created a methods which checks if train departures with the same traindepartures will not be added

--- src/main/java/edu/ntnu/stud/TrainDispatchRecord.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchRecord.java
@@ -1,13 +1,52 @@
 package edu.ntnu.stud;
 
+import java.util.HashMap;
 
 /**
  * Represents a record of train departures.
- * <p> The following functionality is implemented
+ * <p> The following functionality is implemented.
+ * <ul> <li> Add a train departure</li>
+ *
+ *
+ *
+ *
+ *
+ * </ul>
  *
  */
 public class TrainDispatchRecord {
-    private HashMap<int, TrainDeparture> traindepartures;
+  private HashMap<Integer, TrainDeparture> traindepartures;
 
 
+    /**
+     * Creates an instance of TrainDispatchRecord.
+     */
+  public TrainDispatchRecord() {
+      this.traindepartures = new HashMap<>();
+  }
+
+    /**
+     * Adds a train departure to the record.
+     *<p><
+     * This method adds a {@code TrainDeparture} to the record.
+     * if a train departure with the same train number already exists in the record,
+     * the method will return {@code false} and will not add the train number.
+     * if the trian number does not exist in the record, the method will then return {@code true}.
+     * </p>
+     *
+     * @param trainDeparture the train departure to be added.
+     * @return {@code true} if the train departure was added.
+     * @return {@code false} if the train departure was not added.
+     */
+   public boolean addTrainDeparture(TrainDeparture trainDeparture) {
+       int trainNumber = trainDeparture.getTrainNumber();
+       if (this.traindepartures.containsKey(trainNumber)) { //if the train number already exists
+           return false; // if yes return false
+       } else {
+           this.traindepartures.put(trainNumber, trainDeparture);
+           return true; //if no return true, and add the train departure
+       }
+
+      }
+    }
 }
